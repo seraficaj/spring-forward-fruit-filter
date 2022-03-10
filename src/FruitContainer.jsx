9ed92bb97f3c28d95b096e7681b489644036cf4f
Jason Serafica <seraficaj@gmail.com>
2022-03-10 11:20:29 -0800

    render fruits on List

--- src/FruitContainer.jsx
+++ src/FruitContainer.jsx
@@ -2,14 +2,21 @@ import React, { Component } from 'react';
 import Input from './Input';
 import List from './List';
 
+const defaultFruits = ["kumquat", "watermelon", "kiwi", "cantaloupe"]
+
 class FruitContainer extends Component {
     // state managed here for Input and List components
-    state = {  } 
+    state = { 
+        fruitsToDisplay: defaultFruits, // fruits to show in List
+        inputFilterValue: '' // input data from Input
+     } 
     render() { 
         return (
             <>
                 <Input />
-                <List />
+                <List 
+                    fruits={this.state.fruitsToDisplay}
+                />
             </>
         );
     }
--- src/List.jsx
+++ src/List.jsx
@@ -1,9 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class List extends Component {
-    render() { 
-        return (<h1>List Component</h1>);
+    render() {
+        // create a list of fruits from props
+        const fruitListItems = this.props.fruits.map((fruit, idx) => {
+            return <li key={idx}>{fruit}</li>;
+        });
+        return (
+            <>
+                <h2>Fruits List</h2>
+                <ul>{fruitListItems}</ul>
+            </>
+        );
     }
 }
- 
+
 export default List;
